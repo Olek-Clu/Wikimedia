e5c4ba1155902941ca4a027b6f5664f7f6f287f2
oleks <oleksand.khomiakov@hotmail.com>
2026-03-19 19:03:16 -0400

    Add project files.

---
+++ Models/EmailView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class EmailView
+    {
+        public string Email { get; set; }
+    }
+}
